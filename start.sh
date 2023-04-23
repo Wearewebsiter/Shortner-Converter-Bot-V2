e771cfe17bbfe32e691d6e6608e4f20e40e546be
Yaseen Ullal <118463313+Wearewebsiter@users.noreply.github.com>
2023-04-23 21:43:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Cyniteofficial/Shortner-Converter-Bot-V2.git /Shortner-Converter-Bot-V2
-cd /Shortner-Converter-Bot-V2
+git clone https://github.com/Wearewebsiter/Link-Shortner-Converter.git /Link-Shortner-Converter
+cd /Link-Shortner-Converter
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
